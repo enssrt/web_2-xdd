0a064259480bc5a0af22f5c872b1a37d799ed908
enssrt <stalkerised@gmail.com>
2024-05-04 16:31:30 +0300

    add files

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        pager: false,
+        auto: true,
+        controls: false,
+        
+    })
+})
+$(document).ready(function(){
+    $('.bxslider_team').bxSlider({
+        auto: true,
+        controls: false,    
+    })
+})
+$(document).ready(function(){
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 200,
+        itemMargin: 25,
+        touch: true,
+    });
+})
+$(document).ready(function(){
+    $('.accordion').accordion({
+        defaultOpen:""
+    });
+});
+$(function(){
+    let filterList = {
+        init: function(){
+            $('.workportfolio').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap'
+            });
+        },
+    };
+    filterList.init();
+});
